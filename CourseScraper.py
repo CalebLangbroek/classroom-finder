e4a9930b31327001b908a9a71e6c129d9955855e
Donovan K <donovankutzer@gmail.com>
2019-10-25 18:51:33 -0700

    Scraping added session/login

--- CourseScraper.py
+++ CourseScraper.py
@@ -1,17 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+from requests import Session
 
-
-soup = BeautifulSoup
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/72.0.3626.121 Safari/537.36'}
 
-#Before entering term
-url = 'https://warden.ufv.ca:8910/prod/bwskfcls.p_sel_crse_search'
-# URL after entering term: 'https://warden.ufv.ca:8910/prod/bwckgens.p_proc_term_date'
+with Session() as s:
+    # Grab session key for post data
+    site = s.get("https://myportal.ufv.ca/")
+    bs_content = BeautifulSoup(site.content, "html.parser")
+    token = bs_content.find("input", {"name": "sessionDataKey"})["value"]
 
-#Form data input
-post_params = {'p_term': '201909'}
+    # Log in with any student info
+    login_data = {"username": "USERNAME HERE", "password": "PASSWORD HERE", "sessionDataKey": token}
+    s.post("https://identity.ufv.ca/commonauth", login_data)
 
-page = requests.get(url, data = post_params, headers = headers)
-soup = BeautifulSoup(page.content, 'html.parser')
+    # The page we are wanting to scrape
+    home_page = s.get("https://myportal.ufv.ca/")
+    print(home_page.text)
