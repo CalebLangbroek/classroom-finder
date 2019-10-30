7333eb2f6cbe3e0c27431b2cc709fff1587d9257
Caleb Langbroek <34527287+CalebLangbroek@users.noreply.github.com>
2019-10-30 11:59:00 -0700

    Added map, profile, and search fragments

---
+++ app/src/main/java/com/ninjatech/classroomfinder/MapFragment.kt
@@ -0,0 +1,27 @@
+package com.ninjatech.classroomfinder
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ninjatech.classroomfinder.databinding.FragmentMapBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MapFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentMapBinding>(inflater, R.layout.fragment_map, container, false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/ninjatech/classroomfinder/SearchFragment.kt
@@ -0,0 +1,27 @@
+package com.ninjatech.classroomfinder
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ninjatech.classroomfinder.databinding.FragmentSearchBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentSearchBinding>(inflater, R.layout.fragment_search, container, false)
+        return binding.root
+    }
+
+
+}
