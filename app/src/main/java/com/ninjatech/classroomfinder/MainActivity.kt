de5b325accc93ef18a05a975cad0bab4aee7c6a0
Caleb Langbroek <34527287+CalebLangbroek@users.noreply.github.com>
2019-10-30 13:12:23 -0700

    Added navigation Navigation between fragments is working

--- app/src/main/java/com/ninjatech/classroomfinder/MainActivity.kt
+++ app/src/main/java/com/ninjatech/classroomfinder/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.ninjatech.classroomfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
+
+        val bottomNavController = findNavController(R.id.app_nav_host_fragment)
+
+        val appBarConfig =
+            AppBarConfiguration(setOf(R.id.profileFragment, R.id.mapFragment, R.id.searchFragment))
+
+        setupActionBarWithNavController(bottomNavController, appBarConfig)
+        bottomNav.setupWithNavController(bottomNavController)
+
     }
+
 }
