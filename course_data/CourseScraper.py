35f0a256f318cef051f2c8a25ab47f88c8c919a6
Akash Davesar <a.davesar@hotmail.com>
2019-11-07 15:40:58 -0800

    changes for Google Maps API Key

--- course_data/CourseScraper.py
+++ course_data/CourseScraper.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, ssl
 from requests import Session
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.adapters import HTTPAdapter
+
+
+class Adapter(HTTPAdapter):
+    """"Transport adapter" that allows us to use SSLv3."""
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        version = ssl.PROTOCOL_SSLv23
+        self.poolmanager = PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_version=version)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -8,7 +20,8 @@ headers = {
 
 with Session() as s:
     # Grab session key for post data
-    site = s.get("https://myportal.ufv.ca/")
+    s.mount("https://warden.ufv.ca:8910", Adapter())
+    site = s.get("https://warden.ufv.ca:8910/prod/bwysched.p_select_term?wsea_code=CRED")
     bs_content = BeautifulSoup(site.content, "html.parser")
     token = bs_content.find("input", {"name": "sessionDataKey"})["value"]
 
